37440c86ac7e41776742d89ce010e99891b3b843
tsukasa829 <unk_sniper@outlook.jp>
2025-11-22 12:11:54 +0900

    Remove Stripe mention from standard payment page

--- app/product/standerd/payment/page.tsx
+++ app/product/standerd/payment/page.tsx
@@ -72,7 +72,7 @@ export default function StandardPaymentPage() {
               </button>
             )}
 
-            <p className="text-xs text-gray-600 text-center">お支払いリンクは安全なStripe決済を利用します</p>
+            
           </div>
         </div>
       </div>
